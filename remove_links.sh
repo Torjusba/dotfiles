913123b744b2bed5779f37a489c0aca552c88710
Torjus <torjus.bakkene@gmail.com>
2019-04-26 16:41:30 +0200

    Fixed clearing list on deletion and output

--- remove_links.sh
+++ remove_links.sh
@@ -4,6 +4,14 @@ TARGET=$HOME
 
 IGNORE_LIST=".linkignore"
 
+
+function echo_red {
+	echo -e "\033[0;31m"$1"\033[0;m"
+}
+function echo_green {
+	echo -e "\033[0;32m"$1"\033[0;m"
+}
+
 echo "Removing all matches in .links"
 echo "Proceed? (y/n)"
 read resp
@@ -15,5 +23,13 @@ fi
 
   
 for i in $(cat .links); do
-		unlink $i
+	if $(unlink $i); then
+		echo_green "Removed link to $i"
+	else
+		echo_red "Could not remove link to $i"
+	fi
 done
+
+# Clear links list
+rm .links
+touch .links
